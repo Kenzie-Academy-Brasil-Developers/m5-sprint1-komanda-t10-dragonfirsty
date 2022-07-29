24dcc4de0f4211c5dc06736db71ef9fe5dec1047
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-07-29 16:11:27 -0300

    Build: imports and new function start

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+from utils.json_handler import read_menu,write_menu
+from management.tab_handler import calculo_da_comanda
+
+FILEPATH = 'menu.json'
+    
+item = { "name": "Bolo", "price": 10.0 }
+table_1 = [{'id': 1, 'amount': 5}, {'id': 19, 'amount': 5}]
+    
+def write_new_item(newItem : dict):
+    database_data = read_menu(FILEPATH)
+    id_key = len(database_data) + 1
+    update_item = {'id' : id_key, 'name': newItem['name'],'price' : newItem['price']}
+    database_data.append(update_item)
+    write_menu(FILEPATH,database_data)
+
 if __name__ == "__main__":
-    from utils.json_handler import read_menu,write_menu
-
-    FILEPATH = 'menu.json'
-    
-    read_menu(FILEPATH)
-
-    item = { "name": "Bolo", "price": 10.0 }
-    
-
-    def write_new_item(newItem : dict):
-        database_data = read_menu(FILEPATH)
-        id_key = len(database_data) + 1
-        newItem['id'] = id_key 
-        update_item = {'id' : id_key, 'name': newItem['name'],'price' : newItem['price']}
-        database_data.append(update_item)
-        write_menu(FILEPATH,database_data)
+    #print(read_menu(FILEPATH))
         
-    write_new_item(item)
+    # write_new_item(item)
+    print(calculo_da_comanda(table_1))
+
